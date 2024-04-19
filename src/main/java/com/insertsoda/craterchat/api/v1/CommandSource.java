fcef19390cc7bee14698b8a12fc384e31112ae1a
InsertSoda <72355883+InsertSoda@users.noreply.github.com>
2024-04-19 14:05:58 +0200

    Make CommandSource.getWorld() a thing
    I implemented it, but didn't add it to the interface, whoops

--- src/main/java/com/insertsoda/craterchat/api/v1/CommandSource.java
+++ src/main/java/com/insertsoda/craterchat/api/v1/CommandSource.java
@@ -1,10 +1,12 @@
 package com.insertsoda.craterchat.api.v1;
 
 import finalforeach.cosmicreach.entities.Player;
+import finalforeach.cosmicreach.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public interface CommandSource {
 
     Player getPlayer();
 
+    World getWorld();
 }
